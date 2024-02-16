6f4e1dd882548a87242176834c2d44ca01e728dd
Remzi ŞAHBAZ <remzi.sahbaz@tim.org.tr>
2024-02-16 16:36:56 +0300

    add CustomWorkItemHandler

---
+++ custom-task-for-work-item-handler-on-jbpm-java-app/src/main/java/com/remzisahbaz/CustomWorkItemHandler.java
@@ -0,0 +1,26 @@
+package com.remzisahbaz;
+
+import java.util.*;
+
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+public class CustomWorkItemHandler implements WorkItemHandler {
+
+    @Override
+    public void executeWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+        workItemManager.abortWorkItem(workItem.getId());
+    }
+
+    @Override
+    public void abortWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+        System.out.println("******************************************CustomWorkItemHandler**************************************");
+        Map<String,Object> parameters;
+        parameters =  workItem.getParameters();
+        parameters.entrySet().forEach(System.out::println);
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put("Result", "Executed CustomWIH successfully by remzisahbaz");
+        workItemManager.completeWorkItem(workItem.getId(), resultMap);
+    }
+}
